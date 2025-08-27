6483ce67aae92427cdc094ce038c6b59c004e1f6
Dominic Foster <domfoster1@gmail.com>
2025-08-27 10:17:35 +0100

    altered event listeners and operand to ensure it works

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,20 @@
-// Wait for the DOM to finish before loading the game
+// Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
 
-document.addEventListener("DOMcontentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
 
-    for(let button of buttons){
-        button.addEventListener("click" function()
-        if (this.getAttribute(data-type) === "Submit") {
-            alert("You clicked submit!")
-        } else {
-            let gameType = this.getAttribute("data-type");
-            alert("You clicked $gameType");
-        }
-    };
-}
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
 
 function runGame() {
 
@@ -27,26 +28,22 @@ function calculateCorrectAnswer() {
 
 }
 
-function incrementScore {
+function incrementScore() {
 
 }
 
-function incrementWrongAnswer {
+function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion {
+function displayAdditionQuestion() {
 
 }
 
-function displaySubtractQuestion {
+function displaySubtractQuestion() {
 
 }
 
-function displayMultiplyQuestion {
-
-}
-
-function displayDivisionQuestion {
-
+function displayMultiplyQuestion() {
+    
 }
